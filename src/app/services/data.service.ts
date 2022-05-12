8f5512d8198e9e454816cf3db411416799e609e2
NikitaTkachev <nikitatkachevv@gmail.com>
2022-05-12 11:34:05 +0300

    feature(data): implement data service

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,83 +1,65 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, combineLatest, Observable } from 'rxjs';
+import { debounceTime, filter, map, shareReplay, switchMap } from 'rxjs/operators';
 
-export interface Message {
-  fromName: string;
-  subject: string;
-  date: string;
-  id: number;
-  read: boolean;
-}
+import { IPhoto } from '../models';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
-  public messages: Message[] = [
-    {
-      fromName: 'Matt Chorsey',
-      subject: 'New event: Trip to Vegas',
-      date: '9:32 AM',
-      id: 0,
-      read: false
-    },
-    {
-      fromName: 'Lauren Ruthford',
-      subject: 'Long time no chat',
-      date: '6:12 AM',
-      id: 1,
-      read: false
-    },
-    {
-      fromName: 'Jordan Firth',
-      subject: 'Report Results',
-      date: '4:55 AM',
-      id: 2,
-      read: false
-    },
-    {
-      fromName: 'Bill Thomas',
-      subject: 'The situation',
-      date: 'Yesterday',
-      id: 3,
-      read: false
-    },
-    {
-      fromName: 'Joanne Pollan',
-      subject: 'Updated invitation: Swim lessons',
-      date: 'Yesterday',
-      id: 4,
-      read: false
-    },
-    {
-      fromName: 'Andrea Cornerston',
-      subject: 'Last minute ask',
-      date: 'Yesterday',
-      id: 5,
-      read: false
-    },
-    {
-      fromName: 'Moe Chamont',
-      subject: 'Family Calendar - Version 1',
-      date: 'Last Week',
-      id: 6,
-      read: false
-    },
-    {
-      fromName: 'Kelly Richardson',
-      subject: 'Placeholder Headhots',
-      date: 'Last Week',
-      id: 7,
-      read: false
-    }
-  ];
 
-  constructor() { }
+  // Filter criteria
+  private readonly filterSubject = new BehaviorSubject<string>('');
+  private readonly filterAction$ = this.filterSubject.asObservable();
 
-  public getMessages(): Message[] {
-    return this.messages;
+  // Photos server API
+  private readonly photoUrl = 'https://jsonplaceholder.typicode.com/photos';
+  private readonly allPhotos$: Observable<IPhoto[]> = this.http.get<IPhoto[]>(this.photoUrl).pipe(
+    shareReplay(1),
+  );
+
+  readonly filteredPhotos$ = combineLatest([
+    this.allPhotos$,
+    this.filterAction$.pipe(debounceTime(500)),
+  ]).pipe(
+    map(([photos, filterBy]) => this.performFilter(photos, filterBy))
+  );
+
+  // Total photos
+  readonly totalResults$ = this.filteredPhotos$.pipe(
+    map((photos) => photos.length),
+  );
+
+  // Handle photo selection action
+  private readonly photoSelectedSubject = new BehaviorSubject<string | null>(null);
+  private readonly photoSelectedAction$ = this.photoSelectedSubject.asObservable();
+
+  readonly photo$ = this.photoSelectedAction$.pipe(
+    filter<string>(Boolean),
+    switchMap((selectedId: string) =>
+      this.http.get<IPhoto>(`${this.photoUrl}/${selectedId}`),
+    ),
+  );
+
+  constructor(
+    @Inject(HttpClient) private readonly http: HttpClient,
+  ) { }
+
+  changeFilter(filterBy: string): void {
+    this.filterSubject.next(filterBy);
   }
 
-  public getMessageById(id: number): Message {
-    return this.messages[id];
+  changeSelectedPhoto(selectedPhotoId: string): void {
+    this.photoSelectedSubject.next(selectedPhotoId);
+  }
+
+  private performFilter(photos: IPhoto[], filterBy: string): IPhoto[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return photos.filter(
+      (photo: IPhoto) => photo.title.toLocaleLowerCase().indexOf(filterBy) !== -1
+    );
   }
 }
