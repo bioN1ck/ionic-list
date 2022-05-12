54f7e521cd3f78289ec1e9e8513ff3808997ee2d
NikitaTkachev <nikitatkachevv@gmail.com>
2022-05-12 11:30:22 +0300

    refactor(app): rename the app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'wilson-test',
+  appName: 'ionic-list',
   webDir: 'www',
   bundledWebRuntime: false
 };
